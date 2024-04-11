98c3c3ba0dda42ba8c18d05ec4b5a1642e2abd2a
Adam <adam-c-hayes@hotmail.co.uk>
2024-04-11 20:23:58 +0100

    ;

--- src/Data/Parser/IRESPParser.cs
+++ src/Data/Parser/IRESPParser.cs
@@ -8,6 +8,6 @@ namespace HeronKV.Data.Parser
 {
     internal interface IRESPParser
     {
-        public RESPValue Parse(StringReader reader)
+        public RESPValue Parse(StringReader reader);
     }
 }
